e30a5ed031640a04be33242185d354a9c70383e4
Fanny Reslow <reslowf@gmail.com>
2022-03-06 21:59:04 +0100

    working on meme

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,12 +1,27 @@
 import "./meme.css";
+import data from "../utilities/memesData";
+
 export default function Meme() {
+  let memesarray = data.data.memes;
+
+  function getRandom(items) {
+    let random = items[Math.floor(Math.random() * items.length)];
+    console.log(random.url);
+    return random;
+  }
+
+  function clickhandler() {
+    getRandom(memesarray);
+  }
+
   return (
     <form className="meme--form">
-      <section className="input--section">
-        <input type="text" placeholder="first text" className="input" />
-        <input type="text" placeholder="second text" className="input" />
-      </section>
-      <button className="btn">Get a new meme image</button>
+      <input type="text" placeholder="first text" className="input" />
+      <input type="text" placeholder="second text" className="input" />
+
+      <button className="btn" type="button" onClick={clickhandler}>
+        Get a new meme image
+      </button>
     </form>
   );
 }
