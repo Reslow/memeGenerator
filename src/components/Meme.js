1c3f578430e9038337fe8a940ff867621010ac8a
Fanny Reslow <reslowf@gmail.com>
2022-03-07 14:32:27 +0100

    when btn click I get a random image, using states

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,27 +1,43 @@
 import "./meme.css";
 import data from "../utilities/memesData";
+import { useState } from "react";
 
 export default function Meme() {
-  let memesarray = data.data.memes;
+  const [meme, setMeme] = useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg",
+  });
+  const [memesdata, setMemesData] = useState(data);
 
   function getRandom(items) {
     let random = items[Math.floor(Math.random() * items.length)];
-    console.log(random.url);
+
     return random;
   }
 
   function clickhandler() {
-    getRandom(memesarray);
+    let meme = getRandom(memesdata.data.memes);
+
+    setMeme((oldval) => ({
+      ...oldval,
+      randomImage: meme.url,
+    }));
   }
 
   return (
-    <form className="meme--form">
-      <input type="text" placeholder="first text" className="input" />
-      <input type="text" placeholder="second text" className="input" />
+    <section className="container--section">
+      <form className="meme--form">
+        <input type="text" placeholder="first text" className="input" />
+        <input type="text" placeholder="second text" className="input" />
 
-      <button className="btn" type="button" onClick={clickhandler}>
-        Get a new meme image
-      </button>
-    </form>
+        <button className="btn" type="button" onClick={clickhandler}>
+          Get a new meme image
+        </button>
+      </form>
+      <section className="memeImg--section">
+        <img src={meme.randomImage} alt="random meme" className="memeImage" />
+      </section>
+    </section>
   );
 }
